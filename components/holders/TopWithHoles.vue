9b7bdc256c98decbaf28908a40a389ad735c6bb6
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2019-11-24 17:44:12 -0800

    move Editor to own component; add TopWithHoles component

---
+++ components/holders/TopWithHoles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-gray-600 w-5 h-6"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component({})
+export default class TopWithHoles extends Vue {
+  mounted() {
+    console.log("TopWithHoles mounted!")
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,24 @@
 <template>
   <div id="editor-container" class="bg-gray-300 antialiased min-h-screen flex">
     <div class="w-2/3">
-      <canvas id="editor" />
+      <Editor />
     </div>
-    <div class="w-1/3 bg-gray-500" />
+    <div class="w-1/3 bg-gray-500">
+      <TopWithHoles />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
-import { fabric } from "fabric";
 
-@Component({})
-export default class extends Vue {
-  mounted() {
-    const canvas = new fabric.Canvas("editor", {
-      backgroundColor: "rgb(226, 232, 240)", // same color as bg-gray-300
-      height: this.canvasHeight(),
-      width: this.canvasWidth()
-    })
+import Editor from "../components/Editor.vue";
+import TopWithHoles from "../components/holders/TopWithHoles.vue";
 
-    // create a rectangle object
-    const rect = new fabric.Rect({
-      fill: "red",
-      width: 80,
-      height: 80,
-      hasControls: true
-    })
-
-    canvas.add(rect)
-  }
-
-  // Returns screen width * 2/3 which is the allowed max size for editor.
-  canvasWidth(): number {
-    const width =
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth
-
-    return (width * 2) / 3
-  }
-
-  // Returns screen height.
-  canvasHeight(): number {
-    return window.innerHeight ||
-      document.documentElement.clientHeight ||
-      document.body.clientHeight
-  }
-}
+@Component({
+  components: { Editor, TopWithHoles }
+})
+export default class extends Vue { }
 </script>
 
 <style scoped></style>
